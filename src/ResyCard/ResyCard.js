f60255d08a87a8ea6ec86773642bd72679169a05
Caleb Cyphers <caleb.j.cyphers@gmail.com>
2020-12-16 10:09:36 -0700

    Build resyCard component and display info

---
+++ src/ResyCard/ResyCard.js
@@ -0,0 +1,15 @@
+import React from "react"
+import "./ResyCard.css"
+
+function ResyCard(props) {
+  return(
+    <article className="ResyCard" key={props.reservation.id}>
+      <h3>{props.reservation.name}</h3>
+      <p>{props.reservation.date}</p>
+      <p>{props.reservation.time}</p>
+      <p>Number of guests: {props.reservation.number}</p>
+    </article>
+  )
+}
+
+export default ResyCard;
