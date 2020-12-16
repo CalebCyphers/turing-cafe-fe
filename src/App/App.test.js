bdb8688b40e3c495da772eee53bfabc24f485267
Caleb Cyphers <caleb.j.cyphers@gmail.com>
2020-12-16 12:09:49 -0700

    Add tests for app (unfinished)

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { render, screen } from '@testing-library/react'
+import userEvent from "@testing-library/user-event";
+import '@testing-library/jest-dom'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe("ResyForm", () => {
+  it("Should display the form", () => {
+    render(<App />)
+
+    let nameInput = screen.getByPlaceholderText(/name/i)
+    let dateInput = screen.getByPlaceholderText(/date/i)
+    let timeInput = screen.getByPlaceholderText(/time/i)
+    let numberOfGuestsInput = screen.getByPlaceholderText(/number of guests/i)
+    let reservationButton = screen.getByRole('button', { name: /make reservation/i })
+
+    expect(nameInput).toBeInTheDocument()
+    expect(dateInput).toBeInTheDocument()
+    expect(timeInput).toBeInTheDocument()
+    expect(numberOfGuestsInput).toBeInTheDocument()
+    expect(reservationButton).toBeInTheDocument()
+  })
+
+  it("Should be able to make a new card", () => {
+    render(<App />)
+
+  })
+})
