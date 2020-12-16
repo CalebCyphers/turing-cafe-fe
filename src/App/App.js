1c51224e2031aa59259c2f5aaa035befd7f93621
Caleb Cyphers <caleb.j.cyphers@gmail.com>
2020-12-16 09:46:18 -0700

    Add a reservations key in state

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import getReservations from '../apiCalls'
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-
+      reservations: []
     }
   }
   render() {
