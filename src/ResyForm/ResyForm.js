e0dc0c2482c5e28c6373765f982ca31579b2735a
Caleb Cyphers <caleb.j.cyphers@gmail.com>
2020-12-16 10:34:44 -0700

    Make resyform state reflect inputs

--- src/ResyForm/ResyForm.js
+++ src/ResyForm/ResyForm.js
@@ -10,13 +10,18 @@ class ResyForm extends Component {
       numberOfGuests: null,
     };
   }
+
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
+
   render() {
     return (
       <form>
-        <input name="name" placeholder="name"></input>
-        <input name="date" placeholder="date"></input>
-        <input name="time" placeholder="time"></input>
-        <input name="numberOfGuests" placeholder="number of guests"></input>
+        <input name="name" onChange={this.handleChange} value={this.state.name} placeholder="name"></input>
+        <input name="date" onChange={this.handleChange} value={this.state.date} placeholder="date"></input>
+        <input name="time" onChange={this.handleChange} value={this.state.time} placeholder="time"></input>
+        <input name="numberOfGuests" onChange={this.handleChange} value={this.state.numberOfGuests} placeholder="number of guests"></input>
         <button>Make Reservation</button>
       </form>
     );
